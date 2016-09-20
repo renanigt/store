ba64f505f7310f38767c9e0f54d746d7c5f1f88b
Renan Montenegro <renanigt@gmail.com>
2016-09-20 02:25:07 -0300

    Adding more test to CategoriesController

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController do
 
-  describe "GET index" do
+  describe "GET #index" do
 
     it "assgins @categories" do
       category = Category.create(title: "Technology")
       get :index
       expect(assigns(:categories)).to eq([category])
+    end
+
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template(:index)
     end
 
     it "has status success" do
