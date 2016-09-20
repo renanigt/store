1c4942cc0688b7741dcad0d1747bd15416f853a5
Renan Montenegro <renanigt@gmail.com>
2016-09-20 09:58:18 -0300

    Adding more tests to CategoriesController

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CategoriesController do
   describe "GET #index" do
 
     it "assgins @categories" do
-      category = Category.create(title: "Technology")
+      category = Category.create(title: "Book")
       get :index
       expect(assigns(:categories)).to eq([category])
     end
@@ -17,6 +17,27 @@ RSpec.describe CategoriesController do
 
     it "has status success" do
       get :index
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+  describe "GET #show" do
+
+    let(:category) { Category.create(title: "Electronics") }
+
+    it "assigns @category" do
+      get :show, id: category
+      expect(assigns(:category)).to eq(category)
+    end
+
+    it "renders the :show view" do
+      get :show, id: category
+      expect(response).to render_template(:show)
+    end
+
+    it "has status success" do
+      get :show, id: category
       expect(response).to have_http_status(:success)
     end
 
