f4c780545242372f47dd9082c5da99b854df2022
Renan Montenegro <renanigt@gmail.com>
2016-09-21 00:39:27 -0300

    Adding some tests to model Shop

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Shop do
+  
+  describe "validation" do
+
+    let(:shop) { Shop.new(name: "", description: "") }
+
+    it "is invalid" do
+      expect(shop).to be_invalid
+    end
+
+    it "name has errors" do
+      shop.valid?
+      expect(shop.errors[:name].any?).to be true
+    end
+
+    it "title has errors" do
+      shop.valid?
+      expect(shop.errors[:description].any?).to be true
+    end
+
+    it "is valid" do
+      shop = Shop.new(name: "Any name", description: "Some descripton")
+      expect(shop).to be_valid
+    end
+
+  end
+
 end
